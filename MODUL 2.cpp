20512d2f09b90629e488a1e21add1d60f6f7aff4
Faisal <misbahulfaisal.hilmi@gmail.com>
2024-09-25 16:11:22 +0700

    Add files via upload

---
+++ MODUL 2.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+void enkripsi(){
+
+string plainteks, cipherteks;
+int i, k;
+char c;
+
+cout << "Ketikkan pesan: ";
+cin.ignore();
+getline(cin, plainteks);
+cout << "Masukkan jumlah pergeseran (0-25): ";
+cin >> k;
+
+for(i = 0; i < plainteks.length(); i++){
+    c = plainteks[i];
+    if(isalpha(c)){
+        c = toupper(c);
+        c = c - 65;
+        c = (c + k) % 26;
+        c = c + 65;
+    }
+    cipherteks = cipherteks + c;
+}
+cout << "Cipherteks: " << cipherteks << endl;
+}
+
+void dekripsi(){
+string plainteks, cipherteks;
+int i, k;
+char c;
+
+cout << "Ketikkan cipherteks: ";
+cin.ignore();
+getline(cin, cipherteks);
+cout << "Masukkan jumlah pergeseran (0-25): ";
+cin >> k;
+
+for(i = 0; i < cipherteks.length(); i++){
+    c = cipherteks[i];
+    if(isalpha(c)){
+        c = c - 65;
+        c = (c - k + 26) % 26;
+        c = c + 65;
+        c = tolower(c);
+    }
+    plainteks = plainteks + c;
+}
+cout << "Plainteks: " << plainteks << endl;
+}
+
+int main() {
+int pil; 
+bool stop;
+stop = false;
+while(!stop){
+    cout << "Menu " << endl;
+    cout << "1. enkripsi " << endl;
+    cout << "2. dekripsi " << endl;
+    cout << "3. exit " << endl;
+    cout << "pilih menu "; cin >> pil;
+    switch(pil){
+        case 1: enkripsi(); break;
+        case 2: dekripsi(); break;
+        case 3: stop = true; break;
+}
+}
+}
